c9f817f428821484516e9ac6ecda7936e71e4627
Thrift001 <peterkim8432@gmail.com>
2025-11-01 07:19:20 +0300

    Add drizzle.config.ts

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,8 +1,12 @@
 import { defineConfig } from "drizzle-kit";
 
-const connectionString = process.env.DATABASE_URL;
+const isProd = process.env.NODE_ENV === "production";
+const connectionString = isProd
+  ? process.env.DATABASE_URL
+  : process.env.LOCAL_DATABASE_URL;
+
 if (!connectionString) {
-  throw new Error("DATABASE_URL is required to run drizzle commands");
+  throw new Error("No database connection string found");
 }
 
 export default defineConfig({
